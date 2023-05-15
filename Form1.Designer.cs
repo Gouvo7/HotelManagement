d49d022599b39955ce9a0e7d600a23b6ea00a7f2
Nektarios Gouvousis <gouvousisnektarios@gmail.com>
2023-05-15 16:43:00 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace HotelManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userL = new System.Windows.Forms.Label();
+            this.passL = new System.Windows.Forms.Label();
+            this.userTB = new System.Windows.Forms.TextBox();
+            this.passTB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userL
+            // 
+            this.userL.AutoSize = true;
+            this.userL.Location = new System.Drawing.Point(60, 59);
+            this.userL.Name = "userL";
+            this.userL.Size = new System.Drawing.Size(58, 13);
+            this.userL.TabIndex = 0;
+            this.userL.Text = "Username:";
+            // 
+            // passL
+            // 
+            this.passL.AutoSize = true;
+            this.passL.Location = new System.Drawing.Point(60, 99);
+            this.passL.Name = "passL";
+            this.passL.Size = new System.Drawing.Size(56, 13);
+            this.passL.TabIndex = 0;
+            this.passL.Text = "Password:";
+            // 
+            // userTB
+            // 
+            this.userTB.Location = new System.Drawing.Point(140, 59);
+            this.userTB.MaxLength = 40;
+            this.userTB.Name = "userTB";
+            this.userTB.Size = new System.Drawing.Size(100, 20);
+            this.userTB.TabIndex = 1;
+            // 
+            // passTB
+            // 
+            this.passTB.Location = new System.Drawing.Point(140, 99);
+            this.passTB.MaxLength = 40;
+            this.passTB.Name = "passTB";
+            this.passTB.PasswordChar = '*';
+            this.passTB.Size = new System.Drawing.Size(100, 20);
+            this.passTB.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Είσοδος";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 211);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passTB);
+            this.Controls.Add(this.userTB);
+            this.Controls.Add(this.passL);
+            this.Controls.Add(this.userL);
+            this.Name = "Form1";
+            this.Text = "Είσοδος";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userL;
+        private System.Windows.Forms.Label passL;
+        private System.Windows.Forms.TextBox userTB;
+        private System.Windows.Forms.TextBox passTB;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HotelManagement
+{
+    public partial class Form1 : Form
+    {
+        private string cs = @"server=localhost;userid=root;password=1234;database=hotel";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = userTB.Text;
+            string password = passTB.Text;
+
+            MySqlConnection con = new MySqlConnection(cs); //open connection
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from hot_usr where usr_username = '" + username + "' AND usr_password = '" + password + "'", con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    MessageBox.Show("Successfully Sign In!", "VINSMOKE MJ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Username And Password Not Match!", "VINSMOKE MJ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                userTB.Text = string.Empty;
+                passTB.Text = string.Empty;
+                reader.Close();
+                cmd.Dispose();
+                con.Close();
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Gamhthikes");
+            }
+        }
+    }
+}
